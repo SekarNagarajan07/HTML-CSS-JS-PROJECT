d39c0850991c4afce9ffe5e5d1e01e418a497ed5
sekar N <sekarnagarajan1234@gmail.com>
2023-07-25 17:00:08 +0530

    js

--- Form Validation/script.js
+++ Form Validation/script.js
@@ -19,4 +19,17 @@ function validateInputs() {
 function setError(element, message) {
   const inputGroup = element.parentElement;
   const errorElement = inputGroup.querySelector(".error");
+
+  errorElement.innerText = message;
+  inputGroup.classList.add("error");
+  inputGroup.classList.remove("success");
+}
+
+function setSuccess(element) {
+  const inputGroup = element.parentElement;
+  const errorElement = inputGroup.querySelector(".error");
+
+  errorElement.innerText = "";
+  inputGroup.classList.add("success");
+  inputGroup.classList.remove("error");
 }
