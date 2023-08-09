304b7b3c9cd37a717afe51f7d1c9e2cb5ed14fac
sekar N <sekarnagarajan1234@gmail.com>
2023-08-09 22:24:10 +0530

    js

---
+++ Age Calculator2/script.js
@@ -0,0 +1,22 @@
+const inputE1 = document.querySelector(".input");
+const yearE1 = document.querySelector(".years");
+const monthE1 = document.querySelector(".months");
+const dayE1 = document.querySelector(".days");
+const buttonE1 = document.querySelector("button");
+
+buttonE1.addEventListener("click", calculateAge);
+const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+function calculateAge() {
+  let today = new Date();
+  let inputDateE1 = new Date(inputE1.value);
+  let resultMonth, resultDate, resultYear;
+
+  let inputYear = inputDateE1.getFullYear();
+  let inputMonth = inputDateE1.getMonth() + 1;
+  let inputDate = inputDateE1.getDay();
+
+  let currentYear = today.getFullYear();
+  let currentMonth = today.getMonth() + 1;
+  let currentDate = today.getDate();
+}
